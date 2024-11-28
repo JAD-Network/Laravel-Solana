2f1fca3c10bc33c7e036f344093cd49d36aa880e
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-28 23:51:36 +0000

    Removal of files in old namespaces

---
+++ src/Enums/RPCEndpoint.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JadNetwork\LaravelSolana\Enums;
+
+enum RPCEndpoint: string
+{
+    case DEVNET = "https://api.devnet.solana.com";
+    case TESTNET = "https://api.testnet.solana.com";
+    case MAINNET = "https://api.mainnet-beta.solana.com";
+
+}
