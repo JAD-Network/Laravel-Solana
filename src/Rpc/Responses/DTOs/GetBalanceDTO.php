d43152e7f7aa189361a903f12c32a3d87ee83700
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-12-01 04:01:18 +0000

    Base Request & Response parameters reorder
    Signed-off-by: Dylan Marriott <dylan@onyxleeds.co.uk>

--- src/Rpc/Responses/DTOs/GetBalanceDTO.php
+++ src/Rpc/Responses/DTOs/GetBalanceDTO.php
@@ -14,11 +14,11 @@ class GetBalanceDTO implements IResponseDTO
      * @deprecated Please do not use this constructor directly. Use the array for initialization instead.
      */
     public function __construct(
-        protected int $id = 1,
-        protected string $jsonrpc = "2.0",
         public int $slot,
-        public ?string $apiVersion,
         public readonly string $balance,
+        protected int $id = 1,
+        protected string $jsonrpc = '2.0',
+        public ?string $apiVersion = null,
         public ?Carbon $timestamp = null,
     ) {
         $this->timestamp ??= Carbon::now();
@@ -26,6 +26,6 @@ class GetBalanceDTO implements IResponseDTO
 
     public static function fromResponseObject(Collection $responseObject): IResponseDTO
     {
-        return new self(slot: 49495, apiVersion: '5555', balance: '5kgkfkad');
+        return new self(slot: 49495, balance: '5kgkfkad', apiVersion: '5555');
     }
 }
