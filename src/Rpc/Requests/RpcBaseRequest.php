f94dc4e05aa7e385f3a0bddcc77952f98454b3f2
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-12-01 01:21:18 +0000

    Base Request added abstract createDto method

--- src/Rpc/Requests/RpcBaseRequest.php
+++ src/Rpc/Requests/RpcBaseRequest.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace JadNetwork\LaravelSolana\Rpc\Requests;
 
+use JadNetwork\LaravelSolana\Rpc\SolanaConnector;
 use Saloon\Contracts\Body\HasBody;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Http\Response;
 use Saloon\Traits\Body\HasJsonBody;
 
 abstract class RpcBaseRequest extends Request implements HasBody
 {
     use HasJsonBody;
     protected Method $method = Method::POST;
+    protected string $connector = SolanaConnector::class;
 
     public function __construct(
         protected string $methodName,
         protected array $params = [],
     ) {}
+
+    abstract public function createDtoFromResponse(Response $response): mixed;
 
     /**
      * Solano RPC Endpoints are static for all requests.
